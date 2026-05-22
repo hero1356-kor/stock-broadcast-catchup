7cc229ef108a86fd48e80788e6bf8e42eb2d406d
hero1356-kor <61398815+hero1356-kor@users.noreply.github.com>
2026-05-22 21:44:43 +0900

    Make debug APK easier to install

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,15 @@ android {
         applicationId = "com.yeongung.stockbroadcastcatchup"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
     }
 
     compileOptions {
